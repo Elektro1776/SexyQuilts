57517b29d2b19650fb41b85d1c969e98d71bb8c1
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-08-15 13:06:18 -0600

    setting shit up

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var exbhbs = require('express-handlebars');
+var exphbs = require('express-handlebars');
+var path = require('path');
 var app = express();
 var port = process.env.PORT || 3000
+
+app.set('views', __dirname + '/src/views');
+app.engine('handlebars', exphbs({
+  defaultLayout: __dirname + "/src/views/layouts/main.handlebars"
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res)=>{
+  res.render('home.handlebars')
+});
 
 
 
