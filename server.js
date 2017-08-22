8651a2729fd18dc09a287b499bb6d534d5cd241a
Daniel Lundby <daniellundby@Daniels-MacBook-Pro.local>
2017-08-22 11:47:51 -0600

    working away

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var path = require('path');
 var homeRouter = require('./src/controllers/home.js');
+var aboutRouter = require('./src/controllers/about.js');
+var ordersRouter = require('./src/controllers/orders.js');
+var contactRouter = require('./src/controllers/contact.js');
 var app = express();
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 app.set('views', __dirname + '/src/views');
 app.engine('handlebars', exphbs({
@@ -12,9 +15,12 @@ app.engine('handlebars', exphbs({
   partialsDir: __dirname + '/src/views/partials/'
 }));
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(homeRouter)
+app.use(homeRouter);
+app.use(aboutRouter);
+app.use(ordersRouter);
+app.use(contactRouter);
 
 
 
@@ -23,4 +29,4 @@ app.use(homeRouter)
 
 app.listen(port, ()=>{
   console.log('served started on port', port);
-})
+});
